16f31a6d5643c05214b8bfea200d8b599afe09b6
Amber Zimmerman <ajz1221@gmail.com>
2022-03-04 19:52:01 -0700

    ran first successful test in employee

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -3,8 +3,16 @@ const Employee = require("../lib/employee");
 describe("Employee class", () => {
   describe("getName method", () => {
     it("returns a name", () => {
-    console.log("test")
-
+    const employee = new Employee("Amber", 22, "ajz1221@gmail.com");
+    console.log(employee.getName());
+    expect(employee.getName()).toBe("Amber")
+        });
     });
+    describe("getId method", () => {
+        it("returns a name", () => {
+        const employee = new Employee("Amber", 22, "ajz1221@gmail.com");
+        console.log(employee.getName());
+        expect(employee.getName()).toBe("Amber")
+        });
 });
-});
+
