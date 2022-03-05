c5e2a3912a54975dc02e7f317f58060d9158d5d6
Amber Zimmerman <ajz1221@gmail.com>
2022-03-04 20:27:46 -0700

    completing intern tests

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -1,1 +1,18 @@
-const intern = require("../lib/intern")
+const Intern = require("../lib/intern")
+
+describe("Intern class", () => {
+    describe("getSchool method", () => {
+      it("returns school", () => {
+      const intern = new Intern("UCF");
+      console.log(intern.getSchool());
+      expect(intern.getSchool()).toBe("UCF")
+          });
+      });
+        describe("getRole method", () => {
+          it("returns role", () => {
+          const intern = new Intern("Intern");
+          console.log(intern.getRole());
+          expect(intern.getRole()).toBe("Intern")
+              });
+          });
+});
