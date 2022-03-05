55cce11e30c0a36bd0e5ccc4a1f7f388acf8499f
Amber Zimmerman <ajz1221@gmail.com>
2022-03-04 19:22:14 -0700

    adding jest as a devdependecy and inquirer as a dependency

---
+++ tests/employee.test.js
@@ -0,0 +1,10 @@
+const Employee = require("../lib/employee");
+
+describe("Employee class", () => {
+  describe("getName method", () => {
+    it("returns a name", () => {
+    console.log("test")
+
+    });
+});
+});
