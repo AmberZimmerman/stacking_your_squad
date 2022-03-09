fd8dff13686836871d828bd85fbc995b67b5d55a
Amber Zimmerman <ajz1221@gmail.com>
2022-03-08 18:21:56 -0700

    adding styling

--- index.js
+++ index.js
@@ -46,10 +46,11 @@ const finishedCards = generateEmployeeCards(firstPromptAnswers);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stack your Squad</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css" />
    
 </head>
 
-<body>
+<body class="page">
 <div class="row">
 ${finishedCards}
 </div>
